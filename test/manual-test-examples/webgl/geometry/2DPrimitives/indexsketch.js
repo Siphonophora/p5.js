8c4647ada03574b8e9640fdae321d7c4c2d5519b
Siphonophora <michael.j.conrad+github@gmail.com>
2017-10-03 23:57:57 -0400

    basic side by side sketch complete

--- test/manual-test-examples/webgl/geometry/2DPrimitives/indexsketch.js
+++ test/manual-test-examples/webgl/geometry/2DPrimitives/indexsketch.js
@@ -1,15 +1,35 @@
-
+var testID = 1 
+var setStroke = true;
+var setFill = true;
+var nTests = 9
 
 function setup(){
- 
+	refreshTests();
 }
 
 
-function mouseClicked(){
-	console.log('click');
+function refreshTests(){
 
-
-window.frames['2d'].location.reload();
-
-
+	window.frames['2d'].location = '2d.html?testID=' + testID + '&setStroke=' + setStroke + '&setFill=' + setFill
+	window.frames['3d'].location = '3d.html?testID=' + testID + '&setStroke=' + setStroke + '&setFill=' + setFill
 }
+
+function keyPressed() {
+	if (keyCode === 83) {
+		setStroke = setStroke ? false : true;
+		refreshTests()
+	} 
+
+	if (keyCode === 70) {
+		setFill = setFill ? false : true;
+		refreshTests()
+	}
+
+	if (keyCode === 32) {
+		testID++;
+		testID = testID > nTests ? 1 : testID;
+		console.log(testID)
+		refreshTests()
+	}
+
+  }
--- test/manual-test-examples/webgl/geometry/2DPrimitives/sketch.js
+++ test/manual-test-examples/webgl/geometry/2DPrimitives/sketch.js
@@ -1,4 +1,6 @@
-var totalTests = 3
+var totalTests = 4
+var params
+
 
 function setup(){
   if(mode === '3d'){
@@ -8,6 +10,9 @@ function setup(){
     createCanvas(400,400);
     console.log('2d loaded')
   }
+
+  params = getURLParams();
+
 }
 
 function draw(){
@@ -15,7 +20,8 @@ function draw(){
   	translate(200,200)
   }
   background(180)
-  fill(250,100,0);
+  fill(100,250,0);
+  strokeWeight(4)
   stroke(0);
   line(-200,0,200,0)
   line(0,-200,0,200)
@@ -23,28 +29,62 @@ function draw(){
   line( 100,100,100,-100)
   line(-100,100,100,100)
   line(-100,-100,100,-100)
+  stroke(255,0,0)
 
-  if(floor(millis()/1000)/totalTests%3 === 1){
-  	noStroke();
-  } else if (floor(millis()/1000)/totalTests%3 === 2){
-  	noFill();
-  }
+  if(params.setStroke == 'false'){noStroke()}
+  if(params.setFill   == 'false'){noFill()}
 
-  if(showTest(1)){
-  	ellipse(0,0,150,150)	
+
+  if(params.testID == 1){
+  	ellipse(0,0,150,150)	 
   }
   
-  if(showTest(2)){
+  if(params.testID == 2){
   	ellipse(0,0,150,100)	
   }
 
-  if(showTest(3)){
+  if(params.testID == 3){
   	rect(0,0,150,150)
   }
 
-}
+  if(params.testID == 4){
+    arc(0,0,150,150,0,2,OPEN)
+  }
 
-function showTest(n){
-	return floor(millis()/1000)%totalTests + 1 === n
+  if(params.testID == 5){
+    rect(0,0, 175, 125 );
+    rect(0,0,-175, 125 );
+    rect(0,0,-175, -125);
+    rect(0,0, 175, -125);
+  }
+
+  if(params.testID == 6){
+    rect(0,0,  175, 125 , 5);
+    rect(0,0, -175, 125 , 5);
+    rect(0,0, -175, -125, 5);
+    rect(0,0,  175, -125, 5);
+  }
+
+  if(params.testID == 7){
+    rect(0,0,  175, 125 , 5, 10);
+    rect(0,0, -175, 125 , 5, 10);
+    rect(0,0, -175, -125, 5, 10);
+    rect(0,0,  175, -125, 5, 10);
+  }
+
+  if(params.testID == 8){
+    rect(0,0,  175, 125 , 5, 10, 15);
+    rect(0,0, -175, 125 , 5, 10, 15);
+    rect(0,0, -175, -125, 5, 10, 15);
+    rect(0,0,  175, -125, 5, 10, 15);
+  }
+
+  if(params.testID == 9){
+    rect(0,0,  175, 125 , 5, 10, 15, 40, 1);
+    rect(0,0, -175, 125 , 5, 10, 15, 40, 2);
+    rect(0,0, -175, -125, 5, 10, 15, 40, 3);
+    rect(0,0,  175, -125, 5, 10, 15, 40, 4);
+  }
+
 }
 
